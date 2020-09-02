9a4186ff7e53818e9b1cad199c86f235738e8129
vrsvr <rifatvarisov@gmail.com>
2020-09-02 16:18:45 +0500

    Вторая страница

--- js/main.js
+++ js/main.js
@@ -1,97 +1,117 @@
-$(function() {
+$(function () {
 
   // $('#whitePianino').slick({
-//   slidesToShow: 3
-// });
-$(window).on('resize', function(e){
-  // Переменная, по которой узнаем запущен слайдер или нет.
-  // Храним её в data
-  var init = $(".card-box").data('init-slider');
-  // Если мобильный
-  if(window.innerWidth < 1080){
-    // Если слайдер не запущен
-    if(init != 1){
-      // Запускаем слайдер и записываем в data init-slider = 1
-      $('#saveSlider').slick({
-        arrows: false,
-        prevArrow: '<button type="button" class="slide-prev sale__slider-prev">Previous</button>',
-        nextArrow: '<button type="button" class="slide-next sale__slider-next">Next</button>',
-        dots: true,
-        infinite: false,
-        slidesToShow: 1.5,
-        responsive: [{
-          breakpoint: 740,
-          settings: {
-            slidesToShow: 1.5,
-            slidesToScroll: 1,
-          },
-          breakpoint: 500,
-          settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1,
-          }
-        },
-      ]
-      }).data({'init-slider': 1});
-      $('.control-slider').slick({
-        arrows: false,
-        prevArrow: '<button type="button" class="slide-prev sale__slider-prev">Previous</button>',
-        nextArrow: '<button type="button" class="slide-next sale__slider-next">Next</button>',
-        dots: true,
-        infinite: false,
-        slidesToShow: 1
-      }).data({'init-slider': 1});
+  //   slidesToShow: 3
+  // });
+  $(window).on('resize', function (e) {
+    // Переменная, по которой узнаем запущен слайдер или нет.
+    // Храним её в data
+    var init = $(".card-box").data('init-slider');
+    // Если мобильный
+    if (window.innerWidth < 1080) {
+      // Если слайдер не запущен
+      if (init != 1) {
+        // Запускаем слайдер и записываем в data init-slider = 1
+        $('#saveSlider').slick({
+          arrows: false,
+          prevArrow: '<button type="button" class="slide-prev sale__slider-prev">Previous</button>',
+          nextArrow: '<button type="button" class="slide-next sale__slider-next">Next</button>',
+          dots: true,
+          infinite: false,
+          slidesToShow: 1.5,
+          responsive: [{
+            breakpoint: 740,
+            settings: {
+              slidesToShow: 1.5,
+              slidesToScroll: 1,
+            },
+            breakpoint: 500,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+            }
+          }, ]
+        }).data({
+          'init-slider': 1
+        });
+        $('.control-slider').slick({
+          arrows: false,
+          prevArrow: '<button type="button" class="slide-prev sale__slider-prev">Previous</button>',
+          nextArrow: '<button type="button" class="slide-next sale__slider-next">Next</button>',
+          dots: true,
+          infinite: false,
+          slidesToShow: 1
+        }).data({
+          'init-slider': 1
+        });
+        $('.rent-list__inner--slider').slick({
+          arrows: false,
+          dots: false,
+          infinite: false,
+          slidesToShow: 2,
+          responsive: [{
+            breakpoint: 740,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+            }
+          }, ]
+        }).data({
+          'init-slider': 1
+        });
+      }
     }
-  }
-}).trigger('resize');
+  }).trigger('resize');
 
-	//Валидатор форм и маска для форм
-	const offerFormModal = $('.offer-form-modal')
-	offerFormModal.submit(function(e) {
-			e.preventDefault()
-	})
+  //Валидатор форм и маска для форм
+  const offerFormModal = $('.offer-form-modal')
+  offerFormModal.submit(function (e) {
+    e.preventDefault()
+  })
 
-	offerFormModal.validate({
-			errorElement: "",
-			errorPlacement: (error, element) =>
-					error.appendTo(element.parent().parent()),
-			rules: {
-					tel: {
-							maskRu: true
-					}
-			},
-			messages: {
-					name: "",
-					tel: ""
-			},
-			submitHandler: function (form) {
-					const formInstance = $(form)
+  offerFormModal.validate({
+    errorElement: "",
+    errorPlacement: (error, element) =>
+      error.appendTo(element.parent().parent()),
+    rules: {
+      tel: {
+        maskRu: true
+      }
+    },
+    messages: {
+      name: "",
+      tel: ""
+    },
+    submitHandler: function (form) {
+      const formInstance = $(form)
 
-					console.log('submit')
-					$.ajax({
-							type: "POST",
-							url: "mail.php",
-							data: formInstance.serialize()
-					}).done(function() {
-							console.log('DONE')
-							formInput.val("");
-							formInput.siblings().removeClass('active')
-							$('.modal-wrapper-offer .success-message').addClass('show')
-					});
-					return false;
-			}
-	});
-	jQuery.validator.addMethod('maskRu', function(value, element) {
-			console.log(/\+\d{1}\(\d{3}\)\d{3}-\d{4}/g.test(value));
-			return /\+\d{1}\(\d{3}\)\d{3}-\d{4}/g.test(value);
-	});
-	$('[name="tel"]').mask("+7(999)999-9999",{autoclear: false});
+      console.log('submit')
+      $.ajax({
+        type: "POST",
+        url: "mail.php",
+        data: formInstance.serialize()
+      }).done(function () {
+        console.log('DONE')
+        formInput.val("");
+        formInput.siblings().removeClass('active')
+        $('.modal-wrapper-offer .success-message').addClass('show')
+      });
+      return false;
+    }
+  });
+  jQuery.validator.addMethod('maskRu', function (value, element) {
+    console.log(/\+\d{1}\(\d{3}\)\d{3}-\d{4}/g.test(value));
+    return /\+\d{1}\(\d{3}\)\d{3}-\d{4}/g.test(value);
+  });
+  $('[name="tel"]').mask("+7(999)999-9999", {
+    autoclear: false
+  });
 
 });
 
 //Модальные окна на Pure Js
-(function() {
-  
+(function () {
+
   //Вызов окна колбека
   // openCallback = document.querySelector('.openCallback');
   // callbackModal = document.querySelector('.modal-wrapper__callback');
@@ -131,8 +151,8 @@ $(window).on('resize', function(e){
 
   function closeleftMenuModal() {
     if (!leftMenuModal.classList.contains('hidden')) {
-      leftMenuModal.classList.add('animation');    
-      leftMenuModal.addEventListener('transitionend', function(e) {
+      leftMenuModal.classList.add('animation');
+      leftMenuModal.addEventListener('transitionend', function (e) {
         leftMenuModal.classList.add('hidden');
       }, {
         capture: false,
@@ -165,8 +185,8 @@ $(window).on('resize', function(e){
 
   function closeBaseModal() {
     body.classList.remove('noflow');
-    modalWrapper.classList.add('animation');    
-    modalWrapper.addEventListener('transitionend', function(e) {
+    modalWrapper.classList.add('animation');
+    modalWrapper.addEventListener('transitionend', function (e) {
       modalWrapper.classList.add('hidden');
     }, {
       capture: false,
@@ -175,7 +195,7 @@ $(window).on('resize', function(e){
     });
   };
 
-  for(let i = 0; i<modalWrapperClose.length; i++){
+  for (let i = 0; i < modalWrapperClose.length; i++) {
     modalWrapperClose[i].addEventListener('click', () => {
       closeAllModal();
     });
@@ -185,9 +205,9 @@ $(window).on('resize', function(e){
     closeAllModal();
   })
 
-  document.onkeydown = function(e) {
+  document.onkeydown = function (e) {
     e = e || window.event;
-    if (e.key=='Escape'||e.key=='Esc'||e.keyCode==27) {
+    if (e.key == 'Escape' || e.key == 'Esc' || e.keyCode == 27) {
       closeAllModal();
     }
   };
